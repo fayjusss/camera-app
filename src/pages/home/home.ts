a827ad6aa34b971afd08064e9f5651987e5caf1a
Fayjus Salehin <fsalehin@live.com>
2018-01-29 10:13:49 +0200

    Both take picture and gallery function works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -10,29 +9,40 @@ export class HomePage {
 
   public base64Image: string;
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  constructor(private camera: Camera) {
 
   }
 
-
-
-  onTakePhoto(){
-
-    const options: CameraOptions = {
-      quality: 100,
+  onTakePhoto() {
+    this.camera.getPicture({
       sourceType: this.camera.PictureSourceType.CAMERA,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-
-    this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64:
-     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        saveToPhotoAlbum: true
+  //      targetWidth: 1000,
+  //      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
     }, (err) => {
-     // Handle error
-     console.log(err);
+        console.log(err);
+    });
+  }
+
+  onGallery() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+  //      targetWidth: 1000,
+  //      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
     });
   }
 }
